1af1ca44ecbd00b92d30f9dc1645e5eb29b6c20e
Ercy Moreira Neto <fireball.vb@gmail.com>
2014-12-14 17:59:41 -0200

    Aletaracao no posicionamento dos botoes

--- Main.py
+++ Main.py
@@ -25,11 +25,13 @@ class MainWindow(wx.Frame):
 	def InitUI(self):
 		self.CreateStatusBar()
 		self.filemenu = wx.Menu()
-		self.menuConfig = self.filemenu.Append(wx.NewId(), 'Configure...', 'Configura')
+		self.menuConfig = self.filemenu.Append(wx.NewId(),
+						       'Configure...', 'Configura')
 		self.menuExit = self.filemenu.Append(wx.ID_EXIT, "&Sair", "Sai do programa")
 
 		self.aboutMenu = wx.Menu()
-		self.menuSobre = self.aboutMenu.Append(wx.ID_ABOUT, "Sobre", "Informacoes do programa")
+		self.menuSobre = self.aboutMenu.Append(wx.ID_ABOUT,
+						       "Sobre", "Informacoes do programa")
 		
 		self.menuBar = wx.MenuBar()
 		self.menuBar.Append(self.filemenu, "&Arquivo")
@@ -45,7 +47,8 @@ class MainWindow(wx.Frame):
 		sizer = wx.GridBagSizer(5,5)
 
 		sb1 = wx.StaticBox(panel, label="Entrada Sefaz")
-		self.infoEntrada = wx.StaticBox(panel, label="Servico para monitorir lista da sefaz")
+		self.infoEntrada = wx.StaticBox(panel,
+						label="Servico para monitorir lista da sefaz")
 		self.SBoxSEntrada = wx.StaticBoxSizer(sb1,wx.HORIZONTAL)
 		self.btnEntrada = wx.Button(panel, label="Ativar")
 		self.btnEntradaDes = wx.Button(panel, label="Desativar")
@@ -65,10 +68,24 @@ class MainWindow(wx.Frame):
 		self.SBoxEmissaoSefaz.Add(self.btnSefazDes, flag=wx.LEFT)
 
 		sb3 = wx.StaticBox(panel, label="Impressao/Interno")
+		self.SBoxImpressao = wx.StaticBoxSizer(sb3, wx.HORIZONTAL)
+		self.btnImpressao = wx.Button(panel, label="Ativar")
+		self.btnImpressaoDes = wx.Button(panel,label="Desativar")
+		self.btnImpressaoDes.Disable()
+		
+		self.SBoxImpressao.Add(self.btnImpressao, flag=wx.LEFT)
+		self.SBoxImpressao.Add(self.btnImpressaoDes, flag=wx.LEFT)
+		
 
-
-		sizer.Add(self.SBoxSEntrada, pos=(10,1), span=(1,30), flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT, border=10)
-		sizer.Add(self.SBoxEmissaoSefaz, pos=(1,5), span=(1,10), flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT, border=10)
+		sizer.Add(self.SBoxSEntrada, pos=(2,0), span=(1,20),
+			  flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT,
+			  border=10)
+		sizer.Add(self.SBoxEmissaoSefaz, pos=(0,0), span=(1,10),
+			  flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT,
+			  border=10)
+		sizer.Add(self.SBoxImpressao, pos=(1,0), span=(1,10),
+			  flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT,
+			  border=10)
 
 		self.config = ConfigGen(self)
 
@@ -79,7 +96,8 @@ class MainWindow(wx.Frame):
 		self.Close(True)
 
 	def OnAbout(self, e):
-		dlg = wx.MessageDialog( self, "Um gerenciador de NFE da Acpower", "GereNFEPHP", wx.OK)
+		dlg = wx.MessageDialog( self, "Um gerenciador de NFE da Acpower",
+				       "GereNFEPHP", wx.OK)
 		dlg.ShowModal()
 		dlg.Destroy()
 	def OnConfig(self, event):
